589677dcb6225a55262fd373ecde80d91fb1e8b8
ericmartineau <smartytime@gmail.com>
2021-03-22 11:01:14 -0700

    Changing iface back

--- lib/services/sunny.dart
+++ lib/services/sunny.dart
@@ -14,18 +14,18 @@ export 'package:sunny_dart/sunny_get.dart';
 abstract class BuildContextResolver {
   T resolve<T>(BuildContext? context);
 
-  Widget register(BuildContext? context, resolverOrList,
+  Widget register(BuildContext context, resolverOrList,
       {Widget? child, Key? key});
 }
 
 extension BuildContextResolverExt on BuildContextResolver {
-  Widget registerSingleton<T>(BuildContext? context, T item,
+  Widget registerSingleton<T>(BuildContext context, T item,
       {Widget? child, Key? key}) {
     return register(context, [Inst.constant(item)], child: child, key: key);
   }
 
   Widget registerBuilder<T>(
-      BuildContext? context, T create(BuildContext? context),
+      BuildContext context, T create(BuildContext? context),
       {Widget? child, Key? key, InstDispose<T>? dispose}) {
     return register(context, [Inst.factory(create, dispose: dispose)],
         child: child, key: key);
